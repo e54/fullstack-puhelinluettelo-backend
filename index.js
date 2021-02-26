c78acc13e2fe5c6acd5b5d0297ab120ec8ea77ae
esa.kortelainen <esa.kortelainen@finago.com>
2021-02-26 11:53:50 +0200

    ju

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('content', req => JSON.stringify(req.body) )
 
